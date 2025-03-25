0afe1e4a90b91983c378e2f27dd0f0ae40ac21db
idoz <idon951753@gmail.com>
2025-03-25 20:04:03 +0200

    react_components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Lobby from './components/Lobby';
 import CodeBlock from './components/CodeBlock';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Lobby />} />
-          <Route path="/code-block/:id" element={<CodeBlock />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/code-block/:id" element={<CodeBlock />} />
+      </Routes>
+    </div>
   );
 }
 
