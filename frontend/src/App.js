bfe7e65c8b99a2cf7dfdff3dfe42da27305f1359
idoz <idon951753@gmail.com>
2025-03-26 19:15:11 +0200

    stage_B_logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Lobby from './pages/Lobby/Lobby';
 import CodeBlock from './pages/CodeBlock/CodeBlock';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Lobby />} />
-          <Route path="/code-block/:id" element={<CodeBlock />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/code-block/:id" element={<CodeBlock />} />
+      </Routes>
+    </div>
   );
 }
 
