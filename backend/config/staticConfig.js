eeb6c5b0646b4903d0e6d9ff120c57038e920352
idoz <idon951753@gmail.com>
2025-03-26 23:19:14 +0200

    stage_B_Static_config

--- backend/config/staticConfig.js
+++ backend/config/staticConfig.js
@@ -3,9 +3,9 @@ const express = require('express');
 
 const setupStaticFiles = (app) => {
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../../frontend/build')));
+        app.use(express.static(path.join(__dirname, '../frontend/build')));
         app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../../frontend/build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
         });
     }
 };
