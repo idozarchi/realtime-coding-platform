515896e93633357a5b62ceae130eddfceff5d187
idoz <idon951753@gmail.com>
2025-03-27 00:36:11 +0200

    stage_C_axius_fix

--- frontend/src/components/CodeBlockCard/CodeBlockCard.js
+++ frontend/src/components/CodeBlockCard/CodeBlockCard.js
@@ -5,7 +5,7 @@ const CodeBlockCard = ({ block, onClick }) => {
     return (
         <li
             className="code-block-item"
-            onClick={() => onClick(block._id)}
+            onClick={() => onClick(block.id)}
         >
             <h2>{block.name}</h2>
             <p>Click to start coding</p>
